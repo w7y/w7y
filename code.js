08d464baf9bc2e67ee98df130c22d4614b3c1164
w7y <99598897+w7y@users.noreply.github.com>
2022-02-18 08:56:48 +0000

    Create code.js

---
+++ code.js
@@ -0,0 +1,26 @@
+var keywords = [
+                   "goog", 
+                   "nujabes", 
+                   "nujabesX"
+               ];
+var links = [
+               "https://www.google.com", 
+               "https://www.youtube.com/watch_videos?video_ids=2sML2bq_WGw,8iP3J8jFYdM,7xZY8VJHqU4",
+               "https://www.youtube.com/watch_videos?video_ids=5yYMicbSf98,PEWZL6VPBiA,lX8QVBl-ZG0,0Jg74p6D2iU"
+            ]
+
+document.getElementById("text1").innerHTML="";
+document.getElementById("text2").innerHTML="";
+document.getElementById("text3").innerHTML="";
+document.getElementById("text4").innerHTML="";
+
+for (let i=0;i<keywords.length;i++) 
+{
+    if(window.location.href.includes(keywords[i]))
+    {
+        if(!safe)
+            window.location.replace(links[i]);
+        else
+            document.getElementById("text").innerHTML=links[i];
+    }
+}
