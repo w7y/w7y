629621a65d3c4d71633d8d5e8b0620a4e01b176e
w7y <99598897+w7y@users.noreply.github.com>
2022-02-18 09:36:54 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,34 +1,24 @@
-var keywords = [
-                   "goog", 
-                   "nujabes", 
-                   "nujabesX"
-               ];
 var links = [
-               "https://www.google.com", 
-               "https://www.youtube.com/watch_videos?video_ids=2sML2bq_WGw,8iP3J8jFYdM,7xZY8VJHqU4",
-               "https://www.youtube.com/watch_videos?video_ids=5yYMicbSf98,PEWZL6VPBiA,lX8QVBl-ZG0,0Jg74p6D2iU"
-            ]
+               "goog", "https://www.google.com",
+               "nujabes", "https://www.youtube.com/watch_videos?video_ids=2sML2bq_WGw,8iP3J8jFYdM,7xZY8VJHqU4",
+               "nujabesX", "https://www.youtube.com/watch_videos?video_ids=5yYMicbSf98,PEWZL6VPBiA,lX8QVBl-ZG0,0Jg74p6D2iU"
+            ];
 
 let loop = null;
 loop = setInterval(() => {
-
-    for (let i=0;i<keywords.length;i++) 
+    for(let i=0;i<links.length;i++) 
     {
-        if(window.location.href.includes(keywords[i]))
+        if(window.location.href.includes(links[i]))
         {
             document.getElementById("text").innerHTML=":)";
-            document.getElementById("text1").innerHTML="";
-            document.getElementById("text2").innerHTML="";
-            document.getElementById("text3").innerHTML="";
-            document.getElementById("text4").innerHTML="";
+            document.getElementById("text1").innerHTML=document.getElementById("text2").innerHTML=document.getElementById("text3").innerHTML=document.getElementById("text4").innerHTML="";
 
             if(!window.location.href.includes("?safe"))
-                window.location.replace(links[i]);
+                window.location.replace(links[i+1]);
             else
-                document.getElementById("text").innerHTML=links[i];
+                document.getElementById("text").innerHTML=links[i+1];
           
             clearInterval(loop);
         }
     }
-
 }, 500);
